0a7ce984ada73e22d5bf52aaf150f480e274221f
arthurmov <officialamovsesyan@gmail.com>
2025-06-23 16:57:09 -0400

    Added additional method signatures to shoppingcartdao + Created POST, PUT and DELETE methods in shopping cart controller

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,15 @@
 package org.yearup.data;
 
+import org.yearup.models.Category;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    ShoppingCartItem create(Product product);
+    void update(int userId, Product product);
+    void delete(int userId);
 }
