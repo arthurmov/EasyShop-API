bc8f572bc65d21e8734cbaa4e8b69a5357f25f44
arthurmov <officialamovsesyan@gmail.com>
2025-06-24 23:37:47 -0400

    working on methods in profile controller

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,4 +1,24 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+
+@RestController
 public class ProfileController {
+    private ProfileDao profileDao;
+    private UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
+
+    public Profile getById()
+    {
+        return null;
+    }
 }
