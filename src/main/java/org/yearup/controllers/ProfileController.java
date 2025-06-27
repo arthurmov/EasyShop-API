3c57224ffb3337a0a30ab5ebd7ffde1b42be8a1f
arthurmov <officialamovsesyan@gmail.com>
2025-06-26 23:12:45 -0400

    working on profilecontroller methods

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,24 +1,33 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.ProfileDao;
 import org.yearup.data.UserDao;
 import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
 
 @RestController
+@RequestMapping("/profile")
 public class ProfileController {
     private ProfileDao profileDao;
     private UserDao userDao;
 
-    @Autowired
-    public ProfileController(ProfileDao profileDao, UserDao userDao) {
-        this.profileDao = profileDao;
-        this.userDao = userDao;
+    @GetMapping
+    public Profile getById(Principal principal)
+    {
+        String username = principal.getName();
+        User user = userDao.getByUserName(username);
+        int userId = user.getId();
+
+        return profileDao.getByUserId(userId);
     }
 
-    public Profile getById()
+    @PutMapping
+    public void updateProfile(@RequestBody Profile profile, Principal principal)
     {
-        return null;
+        profileDao.update(profile.getUserId(), profile);
     }
 }
